2e10cda08bd1701a56833c8791c9358fd0feffad
Victor Garcia Mestre <vgmestre@gmail.com>
2022-01-24 21:51:13 +0100

    wrap app with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
+import { AppProvider } from './context/app-context';
 import Cell from './components/Cell';
+import Grid from './components/Grid';
 
 export function App() {
   return (
-    <div>
-      <h1>Wordle</h1>
-      <Cell />
-    </div>
+    <AppProvider>
+      <div id="screen">
+        <h1>Wordle</h1>
+        <Grid />
+      </div>
+    </AppProvider>
   );
 }
