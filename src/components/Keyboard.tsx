387c61d9f42e5f0f42ea5ff1ee9f834309201fc7
Victor Garcia Mestre <vgmestre@gmail.com>
2022-01-26 23:38:41 +0100

    keyboard

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,9 +1,32 @@
+import KeyboardRow from './KeyboardRow';
+
 interface Props {
-  bestColors: Array<any>;
+  bestColors: Map<any, any>;
   onKey: any;
 }
 const Keyboard = ({ bestColors, onKey }: Props) => {
-  return <div id="keyboard"></div>;
+  return (
+    <div id="keyboard">
+      <KeyboardRow
+        bestColors={bestColors}
+        letters="qwertyuiop"
+        onKey={onKey}
+        isLast={false}
+      />
+      <KeyboardRow
+        bestColors={bestColors}
+        letters="asdfghjkl"
+        onKey={onKey}
+        isLast={false}
+      />
+      <KeyboardRow
+        bestColors={bestColors}
+        letters="zxcvbnm"
+        onKey={onKey}
+        isLast={true}
+      />
+    </div>
+  );
 };
 
 export default Keyboard;
---
+++ src/components/KeyboardRow.tsx
@@ -0,0 +1,33 @@
+import Button from './Button';
+
+interface Props {
+  bestColors: Map<any, any>;
+  letters: any;
+  isLast: any;
+  onKey: (buttonKey: string) => void;
+}
+const KeyboardRow = ({ bestColors, letters, isLast, onKey }: Props) => {
+  let buttons = [];
+  if (isLast) {
+    buttons.push(
+      <Button onKey={onKey} key="enter" buttonKey="Enter">
+        Enter
+      </Button>
+    );
+  }
+  for (let letter of letters) {
+    buttons.push(
+      <Button
+        onKey={onKey}
+        color={bestColors.get(letter)}
+        key={letter}
+        buttonKey={letter}
+      >
+        {letter}
+      </Button>
+    );
+  }
+  return <div>{buttons}</div>;
+};
+
+export default KeyboardRow;
